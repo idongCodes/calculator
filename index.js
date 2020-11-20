005fe8b41b08071fd2ef049ecf1d5a40aec3bd89
Idong Essien <idong.essien@comcast.net>
2020-11-20 15:49:07 -0500

    Created functions that took two numbers as arguments, calculated them, then got passed into a higher order function

---
+++ index.js
@@ -0,0 +1,19 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+const calculate = (num1, num2, operation) => {
+    return operation(num1, num2);
+}
